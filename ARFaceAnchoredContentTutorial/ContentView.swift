4dca4aab513887f33f9762b98dfd83144e8d8ca0
Cole Dennis <coleedennis@gmail.com>
2022-09-18 21:34:28 -0700

    Code to anchor the new Face Anchor RCProject in the ARView Scene.

--- ARFaceAnchoredContentTutorial/ContentView.swift
+++ ARFaceAnchoredContentTutorial/ContentView.swift
@@ -24,10 +24,16 @@ struct ARViewContainer: UIViewRepresentable {
         arView.session.run(ARFaceTrackingConfiguration())
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//        let boxAnchor = try! Experience.loadBox()
+        
+        // Load the "Scene" scene from the "AR Face Anchoring Tutorial" File
+        let faceAnchor = try! ARFaceAnchoringTutorial.loadScene()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(boxAnchor)
+        
+        //Add the face anchor to the scene
+        arView.scene.anchors.append(faceAnchor)
         
         return arView
         
