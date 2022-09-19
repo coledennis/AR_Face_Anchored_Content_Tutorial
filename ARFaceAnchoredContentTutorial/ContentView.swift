6aa856982fa3aa72baa26acb0de987084ce0f5ca
Cole Dennis <coleedennis@gmail.com>
2022-09-18 21:44:28 -0700

    Added in Xcode generated Sphere mesh, and anchored it to the user's face.

--- ARFaceAnchoredContentTutorial/ContentView.swift
+++ ARFaceAnchoredContentTutorial/ContentView.swift
@@ -26,14 +26,26 @@ struct ARViewContainer: UIViewRepresentable {
         // Load the "Box" scene from the "Experience" Reality File
 //        let boxAnchor = try! Experience.loadBox()
         
-        // Load the "Scene" scene from the "AR Face Anchoring Tutorial" File
-        let faceAnchor = try! ARFaceAnchoringTutorial.loadScene()
-        
         // Add the box anchor to the scene
 //        arView.scene.anchors.append(boxAnchor)
         
+        
+        
+        // Load the "Scene" scene from the "AR Face Anchoring Tutorial" File
+//        let faceAnchor = try! ARFaceAnchoringTutorial.loadScene()
+        
         //Add the face anchor to the scene
-        arView.scene.anchors.append(faceAnchor)
+//        arView.scene.anchors.append(faceAnchor)
+        
+        
+        
+        let sphereEntity = ModelEntity(mesh: .generateSphere(radius: 0.1), materials: [SimpleMaterial(color: .white, isMetallic: false)])
+        let sphereAnchor = AnchorEntity(.face)
+        
+        sphereAnchor.addChild(sphereEntity)
+        
+        arView.scene.anchors.append(sphereAnchor)
+        
         
         return arView
         
